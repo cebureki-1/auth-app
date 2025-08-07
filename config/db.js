713f645185707bc51c46aef03b5f043fc5ae4782
Unerbaev Imangali <gta.56.98@bk.ru>
2025-08-07 14:40:46 +0500

    done

--- config/db.js
+++ config/db.js
@@ -1,11 +1,10 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'myappdb',
-  password: 'postgresql',
-  port: 5432,
+  connectionString: 'postgresql://ims:EgsP9xwJlDytZJYafJnYmx7Dga4UAd7W@dpg-d2a6s3ur433s73a5m9og-a.oregon-postgres.render.com/finalproject_db_sl55',
+  ssl: {rejectUnauthorized: false}
 });
+console.log(process.env.DATABASE_URL);
+
 
 module.exports = pool;
